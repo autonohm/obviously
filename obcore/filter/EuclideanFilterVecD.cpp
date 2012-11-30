73084a72d9946118742b7e4c746a11abba4e140c
Philipp Koch <phil@Robotik_92.(none)>
2012-11-30 17:01:49 +0100

    Changed name of the EuclideanFilterD to correct spelling. Added the new EuclideanFilterVecD, which works with stl-vector. The function absVector calculates the amount of a vector and works with stl-vector-iterator. This function should be moved into the obcore/math/mathbase.h.

---
+++ obcore/filter/EuclideanFilterVecD.cpp
@@ -0,0 +1,58 @@
+/*
+ * EuclideanFilterVecD.cpp
+ *
+ *  Created on: 30.11.2012
+ *      Author: phil
+ */
+
+#include "obcore/filter/EuclideanFilterVecD.h"
+#include "obcore/math/mathbase.h"
+#include <cmath>
+#include <iostream>
+
+namespace obvious
+{
+
+double absVector(vector<double>::iterator start);
+
+FILRETVAL EuclideanFilterVecD::applyFilter(void)
+{
+	vector<double>::iterator inIter=_input->begin();
+	double abs=0;
+	unsigned int outCtr=0;
+
+	for(unsigned int i=0;i<((unsigned int)_input->size())/3;i++)
+	{
+		abs=absVector(inIter);
+//		abs=euklideanDistanceVecD(&inIter,NULL);
+		if(abs<_threshold)
+		{
+			for(unsigned int j=0;j<3;j++)
+			{
+			_output->push_back(*inIter++);
+			outCtr++;
+			}
+		}
+		else
+			inIter+=3;
+	}
+
+	return(FILTER_OK);
+}
+
+double absVector(vector<double>::iterator start)
+{
+	double abs=0;
+	vector<double>::iterator iter=start;
+	for(unsigned int i=0;i<3;i++)
+	{
+		abs+=*iter*(*iter);
+		iter++;
+	}
+	return(std::sqrt(abs));
+}
+
+
+
+}  //End of namespace obvious
+
