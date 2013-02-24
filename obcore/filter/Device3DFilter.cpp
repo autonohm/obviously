0e2e3e7fd6a4e6129281cc89143d5c1178bb564b
Philipp Koch <phil@phil-mobile>
2013-02-24 20:14:52 +0100

    Added the initial version. Still completely under construction.

---
+++ obcore/filter/Device3DFilter.cpp
@@ -0,0 +1,21 @@
+/*
+ * Device3DFilter.cpp
+ *
+ *  Created on: 21.02.2013
+ *      Author: phil
+ */
+
+#include <cstdlib>
+
+namespace obvious
+{
+
+Device3DData::Device3DData(void)
+{
+	_pointCloud=NULL;
+	_normals=NULL;
+	_mask=NULL;
+	_depthMap=NULL;
+}
+
+}
