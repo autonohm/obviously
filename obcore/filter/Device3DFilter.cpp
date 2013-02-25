3b1e36e4578dec9dd4408167b6ae29d3296289b2
Philipp Koch <phil@phil-base.(none)>
2013-02-25 01:29:21 +0100

    Implemented the applyFilter function. Added more to the class declaration.

--- obcore/filter/Device3DFilter.cpp
+++ obcore/filter/Device3DFilter.cpp
@@ -6,6 +6,9 @@
  */
 
 #include <cstdlib>
+#include <vector>
+#include "Device3DFilter.h"
+#include "obcore/math/mathbase.h"
 
 namespace obvious
 {
@@ -16,6 +19,94 @@ Device3DData::Device3DData(void)
 	_normals=NULL;
 	_mask=NULL;
 	_depthMap=NULL;
+	_validPoints=0;
 }
+
+Device3DData::~Device3DData(void)
+{
+   if(_pointCloud)
+      delete _pointCloud;
+   if(_normals)
+      delete _normals;
+   if(_mask)
+      delete _mask;
+   if(_depthMap)
+      delete _depthMap;
+}
+
+/****************************************************************************************************/
+
+Device3DFilter::Device3DFilter(void)
+{
+   _input=NULL;
+   _output=NULL;
+   _threshold=0.0;
+}
+
+Device3DFilter::~Device3DFilter(void)
+{
+   if(_input)
+      delete(_input);
+   if(_output)
+      delete(_output);
+}
+
+FILRETVAL Device3DFilter::applyFilter(void)
+{
+   if((!_input))//||(!_output))
+   {
+      std::cout<<"\nError! Invalid input pointer!\n";
+      return(FILTER_ERROR);
+   }
+   if((_threshold<0.001)||(_threshold>-0.001))
+   {
+      std::cout<<"\nError! Threshold not initialized!\n";
+      return(FILTER_ERROR);
+   }
+   if(_output)
+      delete _output;
+
+   double* inPcl=_input->getPointCloud();
+   double* inNorm=_input->getNormals();
+
+   std::vector<double> outPcl;
+   std::vector<double> outNorm;
+   double* outDepthmap=new double[640*480];
+   bool* outMask=new double[640*480];
+   double abs=0.0;
+
+   for(unsigned int i=0;i<640*480;i++)
+   {
+      abs =  euklideanDistance<double>(inPcl, NULL, 3);//Point3D::sizeP);
+      if(abs>_threshold)
+      {
+         outMask[i]=false;                  //set mask to false
+         outDepthmap[i]=0.0;                //set DepthMap to NaN
+         inPcl+=3;
+         inNorm+=3;
+         continue;
+      }
+      else
+      {
+         for(unsigned int j=0;j<3;j++)
+         {
+            outPcl.push_back(*inPcl);
+            inPcl++;
+            outNorm.push_back(*inNorm);
+            inNorm++;
+         }
+         outMask[i]=_input->getMask()[i];
+         outDepthmap[i]=_input->getDepthMap()[i];
+      }
+
+   }
+
+   _output=new Device3DData();
+
+   return(FILTER_OK);
+}
+
+
+
 
 }
--- obcore/filter/Device3DFilter.h
+++ obcore/filter/Device3DFilter.h
@@ -21,15 +21,19 @@ public:
 	double* getPointCloud(void){return(_pointCloud);}
 	double* getNormals(void){return(_normals);}
 	double* getDepthMap(void){return(_depthMap);}
-	bool* getMap(void){return(_mask);}
+	bool* getMask(void){return(_mask);}
+	unsigned int getValidPoints(void){return(_validPoints);}
 	void setPointCloud(double* pointCloud){_pointCloud=pointCloud;}
 	void setNormals(double* pointCloud){_pointCloud=pointCloud;}
 	void setDepthMap(double* depthMap){_depthMap=depthMap;}
+	void setMask(bool* mask){_mask=mask;}
+	void setValidPoints(unsigned int validPoints){_validPoints=validPoints;}
 private:
 	double* _pointCloud;
 	double* _normals;
 	double* _depthMap;
 	bool* _mask;
+	unsigned int _validPoints;
 };
 
 
@@ -38,9 +42,14 @@ class Device3DFilter : public IFilter
 public:
 	Device3DFilter(void);
 	~Device3DFilter(void);
-	FILRETVAL setInput()
+	void setInput(Device3DData* input){_input=input;}
+	Device3DData* getOutput(void);
+	FILRETVAL applyFilter(void);
+	void setThreshold(double threshold){_threshold=threshold;}
 private:
-
+	Device3DData* _input;
+	Device3DData* _output;
+	double _threshold;
 };
 
 }
