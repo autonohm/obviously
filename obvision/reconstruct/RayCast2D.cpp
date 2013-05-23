2f144a9a56828bdaecae7ec0f9b2401e0bbce3f1
sm <sm@ohm>
2013-05-23 12:55:15 +0200

    format 2

--- obvision/reconstruct/RayCast2D.cpp
+++ obvision/reconstruct/RayCast2D.cpp
@@ -10,194 +10,194 @@
 namespace obvious
 {
 
-  RayCast2D::RayCast2D()
+RayCast2D::RayCast2D()
+{
+
+}
+
+RayCast2D::~RayCast2D()
+{
+
+}
+
+void RayCast2D::calcCoordsFromCurrentView(TsdGrid* grid, SensorPolar2D* sensor, double* coords, double* normals, unsigned int* cnt)
+{
+  Timer t;
+  *cnt = 0;
+
+  double c[2];
+  double n[2];
+  Matrix M(3,1);
+  Matrix N(3,1);
+  Matrix* T = sensor->getPose();
+  Matrix Ti(3, 3);
+  Ti = T->getInverse();
+  M[2][0] = 1.0;
+  N[2][0] = 0.0; // no translation for normals
+
+  for (unsigned int beam = 0; beam < sensor->getRealMeasurementSize(); beam++)
   {
-
-  }
-
-  RayCast2D::~RayCast2D()
-  {
-
-  }
-
-  void RayCast2D::calcCoordsFromCurrentView(TsdGrid* grid, SensorPolar2D* sensor, double* coords, double* normals, unsigned int* cnt)
-  {
-    Timer t;
-    *cnt = 0;
-
-    double c[2];
-    double n[2];
-    Matrix M(3,1);
-    Matrix N(3,1);
-    Matrix* T = sensor->getPose();
-    Matrix Ti(3, 3);
-    Ti = T->getInverse();
-    M[2][0] = 1.0;
-    N[2][0] = 0.0; // no translation for normals
-
-    for (unsigned int beam = 0; beam < sensor->getRealMeasurementSize(); beam++)
+    if (rayCastFromCurrentView(grid, sensor, beam, c, n)) // Ray returned with coordinates
     {
-      if (rayCastFromCurrentView(grid, sensor, beam, c, n)) // Ray returned with coordinates
+      M[0][0] = c[0];
+      M[1][0] = c[1];
+      N[0][0] = n[0];
+      N[1][0] = n[1];
+      M       = Ti * M;
+      N       = Ti * N;
+      for (unsigned int i = 0; i < 2; i++)
       {
-        M[0][0] = c[0];
-        M[1][0] = c[1];
-        N[0][0] = n[0];
-        N[1][0] = n[1];
-        M       = Ti * M;
-        N       = Ti * N;
-        for (unsigned int i = 0; i < 2; i++)
+        coords[*cnt] = M[i][0];
+        normals[(*cnt)++] = N[i][0];
+      }
+    }
+  }
+
+  LOGMSG(DBG_DEBUG, "Elapsed TSDF projection: " << t.getTime() << "ms");
+  LOGMSG(DBG_DEBUG, "Ray casting finished! Found " << *cnt << " coordinates");
+}
+
+
+bool RayCast2D::rayCastFromCurrentView(TsdGrid* grid, SensorPolar2D* sensor, const unsigned int beam, double coordinates[2], double normal[2])
+{
+  int xDim = grid->getCellsX();
+  int yDim = grid->getCellsY();
+  double cellSize = grid->getCellSize();
+
+  double tr[2];
+  sensor->getPosition(tr);
+
+  double ray[2];
+  double position[2];
+  double position_prev[2];
+
+  sensor->calcRay(beam, ray);
+  ray[0] *= cellSize;
+  ray[1] *= cellSize;
+
+  // Interpolation weight
+  double interp;
+
+  double xmin   = -10e9;
+  double ymin   = -10e9;
+  if(fabs(ray[0])>10e-6) xmin = ((double)(ray[0] > 0.0 ? 0 : (xDim-1)*cellSize) - tr[0]) / ray[0];
+  if(fabs(ray[1])>10e-6) ymin = ((double)(ray[1] > 0.0 ? 0 : (yDim-1)*cellSize) - tr[1]) / ray[1];
+  double idxMin = max(xmin, ymin);
+  idxMin        = max(idxMin, 0.0);
+
+  double xmax   = 10e9;
+  double ymax   = 10e9;
+  if(fabs(ray[0])>10e-6) xmax = ((double)(ray[0] > 0.0 ? (xDim-1)*cellSize : 0) - tr[0]) / ray[0];
+  if(fabs(ray[1])>10e-6) ymax = ((double)(ray[1] > 0.0 ? (yDim-1)*cellSize : 0) - tr[1]) / ray[1];
+  double idxMax = min(xmax, ymax);
+
+  if (idxMin >= idxMax)
+  {
+    return false;
+  }
+
+  double tsdf_prev;
+  position[0] = tr[0] + idxMin * ray[0];
+  position[1] = tr[1] + idxMin * ray[1];
+  grid->interpolateBilinear(position, &tsdf_prev);
+
+  bool found = false;
+  for(int i=idxMin; i<=idxMax; i++)
+  {
+    // Calculate current position
+    memcpy(position_prev, position, 2 * sizeof(*position));
+
+    position[0] += ray[0];
+    position[1] += ray[1];
+
+    double tsdf;
+    if (!grid->interpolateBilinear(position, &tsdf))
+    {
+      continue;
+    }
+
+    // Check sign change
+    if(tsdf_prev > 0 && tsdf_prev < 0.99999 && tsdf < 0)
+    {
+      interp = tsdf_prev / (tsdf_prev - tsdf);
+      found = true;
+      break;
+    }
+
+    tsdf_prev = tsdf;
+  }
+
+  if(!found) return false;
+
+  coordinates[0] = position_prev[0] + ray[0] * interp;
+  coordinates[1] = position_prev[1] + ray[1] * interp;
+
+  if(!grid->interpolateNormal(coordinates, normal))
+    return false;
+
+  return true;
+}
+
+void RayCast2D::calcCoordsAligned(TsdGrid* grid, double* coords, double* normals, unsigned int* cnt)
+{
+  Timer t;
+  *cnt = 0;
+
+  double c[2];
+  double n[2];
+
+  double cellSize = grid->getCellSize();
+  for (unsigned int y=0; y<grid->getCellsY(); y++)
+  {
+    c[0] = cellSize * 0.5;
+    c[1] = y*cellSize + cellSize * 0.5;
+    double tsdf_prev;
+    grid->interpolateBilinear(c, &tsdf_prev);
+    for (unsigned int x=1; x<grid->getCellsX(); x++)
+    {
+      c[0] += cellSize;
+      double tsdf;
+      if(grid->interpolateBilinear(c, &tsdf))
+      {
+        // Check sign change
+        if(tsdf_prev*tsdf < 0 && fabs(tsdf)<0.9999 && fabs(tsdf_prev)<0.99999)
         {
-          coords[*cnt] = M[i][0];
-          normals[(*cnt)++] = N[i][0];
+          double interp = 1-(tsdf_prev / (tsdf_prev - tsdf));
+          coords[*cnt] = c[0] - interp * cellSize;
+          coords[*cnt+1] = c[1];
+          *cnt+=2;
         }
       }
-    }
-
-    LOGMSG(DBG_DEBUG, "Elapsed TSDF projection: " << t.getTime() << "ms");
-    LOGMSG(DBG_DEBUG, "Ray casting finished! Found " << *cnt << " coordinates");
-  }
-
-
-  bool RayCast2D::rayCastFromCurrentView(TsdGrid* grid, SensorPolar2D* sensor, const unsigned int beam, double coordinates[2], double normal[2])
-  {
-    int xDim = grid->getCellsX();
-    int yDim = grid->getCellsY();
-    double cellSize = grid->getCellSize();
-
-    double tr[2];
-    sensor->getPosition(tr);
-
-    double ray[2];
-    double position[2];
-    double position_prev[2];
-
-    sensor->calcRay(beam, ray);
-    ray[0] *= cellSize;
-    ray[1] *= cellSize;
-
-    // Interpolation weight
-    double interp;
-
-    double xmin   = -10e9;
-    double ymin   = -10e9;
-    if(fabs(ray[0])>10e-6) xmin = ((double)(ray[0] > 0.0 ? 0 : (xDim-1)*cellSize) - tr[0]) / ray[0];
-    if(fabs(ray[1])>10e-6) ymin = ((double)(ray[1] > 0.0 ? 0 : (yDim-1)*cellSize) - tr[1]) / ray[1];
-    double idxMin = max(xmin, ymin);
-    idxMin        = max(idxMin, 0.0);
-
-    double xmax   = 10e9;
-    double ymax   = 10e9;
-    if(fabs(ray[0])>10e-6) xmax = ((double)(ray[0] > 0.0 ? (xDim-1)*cellSize : 0) - tr[0]) / ray[0];
-    if(fabs(ray[1])>10e-6) ymax = ((double)(ray[1] > 0.0 ? (yDim-1)*cellSize : 0) - tr[1]) / ray[1];
-    double idxMax = min(xmax, ymax);
-
-    if (idxMin >= idxMax)
-    {
-      return false;
-    }
-
-    double tsdf_prev;
-    position[0] = tr[0] + idxMin * ray[0];
-    position[1] = tr[1] + idxMin * ray[1];
-    grid->interpolateBilinear(position, &tsdf_prev);
-
-    bool found = false;
-    for(int i=idxMin; i<=idxMax; i++)
-    {
-      // Calculate current position
-      memcpy(position_prev, position, 2 * sizeof(*position));
-
-      position[0] += ray[0];
-      position[1] += ray[1];
-
-      double tsdf;
-      if (!grid->interpolateBilinear(position, &tsdf))
-      {
-        continue;
-      }
-
-      // Check sign change
-      if(tsdf_prev > 0 && tsdf_prev < 0.99999 && tsdf < 0)
-      {
-        interp = tsdf_prev / (tsdf_prev - tsdf);
-        found = true;
-        break;
-      }
-
       tsdf_prev = tsdf;
     }
-
-    if(!found) return false;
-
-    coordinates[0] = position_prev[0] + ray[0] * interp;
-    coordinates[1] = position_prev[1] + ray[1] * interp;
-
-    if(!grid->interpolateNormal(coordinates, normal))
-      return false;
-
-    return true;
   }
 
-  void RayCast2D::calcCoordsAligned(TsdGrid* grid, double* coords, double* normals, unsigned int* cnt)
+  for (unsigned int x=0; x<grid->getCellsX(); x++)
   {
-    Timer t;
-    *cnt = 0;
-
-    double c[2];
-    double n[2];
-
-    double cellSize = grid->getCellSize();
-    for (unsigned int y=0; y<grid->getCellsY(); y++)
+    c[0] = x*cellSize + cellSize * 0.5;
+    c[1] = cellSize * 0.5;
+    double tsdf_prev;
+    grid->interpolateBilinear(c, &tsdf_prev);
+    for (unsigned int y=1; y<grid->getCellsY(); y++)
     {
-      c[0] = cellSize * 0.5;
-      c[1] = y*cellSize + cellSize * 0.5;
-      double tsdf_prev;
-      grid->interpolateBilinear(c, &tsdf_prev);
-      for (unsigned int x=1; x<grid->getCellsX(); x++)
+      c[1] += cellSize;
+      double tsdf;
+      if(grid->interpolateBilinear(c, &tsdf))
       {
-        c[0] += cellSize;
-        double tsdf;
-        if(grid->interpolateBilinear(c, &tsdf))
+        // Check sign change
+        if(tsdf_prev*tsdf < 0 && fabs(tsdf)<0.9999 && fabs(tsdf_prev)<0.99999)
         {
-          // Check sign change
-          if(tsdf_prev*tsdf < 0 && fabs(tsdf)<0.9999 && fabs(tsdf_prev)<0.99999)
-          {
-            double interp = 1-(tsdf_prev / (tsdf_prev - tsdf));
-            coords[*cnt] = c[0] - interp * cellSize;
-            coords[*cnt+1] = c[1];
-            *cnt+=2;
-          }
+          double interp = 1-(tsdf_prev / (tsdf_prev - tsdf));
+          coords[*cnt] = c[0];
+          coords[*cnt+1] = c[1] - interp * cellSize;
+          *cnt+=2;
         }
-        tsdf_prev = tsdf;
       }
+      tsdf_prev = tsdf;
     }
-
-    for (unsigned int x=0; x<grid->getCellsX(); x++)
-    {
-      c[0] = x*cellSize + cellSize * 0.5;
-      c[1] = cellSize * 0.5;
-      double tsdf_prev;
-      grid->interpolateBilinear(c, &tsdf_prev);
-      for (unsigned int y=1; y<grid->getCellsY(); y++)
-      {
-        c[1] += cellSize;
-        double tsdf;
-        if(grid->interpolateBilinear(c, &tsdf))
-        {
-          // Check sign change
-          if(tsdf_prev*tsdf < 0 && fabs(tsdf)<0.9999 && fabs(tsdf_prev)<0.99999)
-          {
-            double interp = 1-(tsdf_prev / (tsdf_prev - tsdf));
-            coords[*cnt] = c[0];
-            coords[*cnt+1] = c[1] - interp * cellSize;
-            *cnt+=2;
-          }
-        }
-        tsdf_prev = tsdf;
-      }
-    }
-    LOGMSG(DBG_DEBUG, "Elapsed TSDF projection: " << t.getTime() << "ms");
-    LOGMSG(DBG_DEBUG, "Ray casting finished! Found " << *cnt << " coordinates");
   }
+  LOGMSG(DBG_DEBUG, "Elapsed TSDF projection: " << t.getTime() << "ms");
+  LOGMSG(DBG_DEBUG, "Ray casting finished! Found " << *cnt << " coordinates");
+}
 
 }
--- obvision/reconstruct/SensorPolar2D.cpp
+++ obvision/reconstruct/SensorPolar2D.cpp
@@ -8,115 +8,115 @@
 namespace obvious
 {
 
-  SensorPolar2D::SensorPolar2D(unsigned int size, double angularRes, double minPhi)
+SensorPolar2D::SensorPolar2D(unsigned int size, double angularRes, double minPhi)
+{
+  _Pose = new Matrix(3, 3);
+  _Pose->setIdentity();
+
+  _size = size;
+  _data = new double[_size];
+  _mask = new bool[_size];
+  for(unsigned int i=0; i<_size; i++)
+    _mask[i] = true;
+
+  _angularRes = angularRes;
+  _minPhi = minPhi;
+
+  if(minPhi>=180.0)
   {
-    _Pose = new Matrix(3, 3);
-    _Pose->setIdentity();
-
-    _size = size;
-    _data = new double[_size];
-    _mask = new bool[_size];
-    for(unsigned int i=0; i<_size; i++)
-      _mask[i] = true;
-
-    _angularRes = angularRes;
-    _minPhi = minPhi;
-
-    if(minPhi>=180.0)
-    {
-      LOGMSG(DBG_ERROR, "Valid minimal angle < 180 degree");
-    }
+    LOGMSG(DBG_ERROR, "Valid minimal angle < 180 degree");
   }
+}
 
-  SensorPolar2D::~SensorPolar2D()
-  {
-    delete _Pose;
-    delete [] _data;
-    delete [] _mask;
-  }
+SensorPolar2D::~SensorPolar2D()
+{
+  delete _Pose;
+  delete [] _data;
+  delete [] _mask;
+}
 
-  void SensorPolar2D::transform(Matrix* T)
-  {
-    (*_Pose) *= (*T);
-  }
+void SensorPolar2D::transform(Matrix* T)
+{
+  (*_Pose) *= (*T);
+}
 
-  Matrix* SensorPolar2D::getPose()
-  {
-    return _Pose;
-  }
+Matrix* SensorPolar2D::getPose()
+{
+  return _Pose;
+}
 
-  void SensorPolar2D::getPosition(double tr[2])
-  {
-    tr[0] = (*_Pose)[0][2];
-    tr[1] = (*_Pose)[1][2];
-  }
+void SensorPolar2D::getPosition(double tr[2])
+{
+  tr[0] = (*_Pose)[0][2];
+  tr[1] = (*_Pose)[1][2];
+}
 
-  unsigned int SensorPolar2D::getRealMeasurementSize()
-  {
-    return _size;
-  }
+unsigned int SensorPolar2D::getRealMeasurementSize()
+{
+  return _size;
+}
 
-  void SensorPolar2D::setRealMeasurementData(double* data)
-  {
-    memcpy(_data, data, _size*sizeof(*data));
-  }
+void SensorPolar2D::setRealMeasurementData(double* data)
+{
+  memcpy(_data, data, _size*sizeof(*data));
+}
 
-  double* SensorPolar2D::getRealMeasurementData()
-  {
-    return _data;
-  }
+double* SensorPolar2D::getRealMeasurementData()
+{
+  return _data;
+}
 
-  void SensorPolar2D::setRealMeasurementMask(bool* mask)
-  {
-    memcpy(_mask, mask, _size*sizeof(*mask));
-  }
+void SensorPolar2D::setRealMeasurementMask(bool* mask)
+{
+  memcpy(_mask, mask, _size*sizeof(*mask));
+}
 
-  bool* SensorPolar2D::getRealMeasurementMask()
-  {
-    return _mask;
-  }
+bool* SensorPolar2D::getRealMeasurementMask()
+{
+  return _mask;
+}
 
-  void SensorPolar2D::calcRay(unsigned int beam, double ray[2])
-  {
-    Matrix Rh(3, 1);
-    double phi = _minPhi + ((double)beam) * _angularRes;
-    double cphi = cos(phi);
-    double sphi = sin(phi);
-    Rh[0][0] = cos(phi);
-    Rh[1][0] = sin(phi);
-    Rh[2][0] = 0.0;
-    Rh = (*_Pose) * Rh;
-    ray[0] = Rh[0][0];
-    ray[1] = Rh[1][0];
-  }
+void SensorPolar2D::calcRay(unsigned int beam, double ray[2])
+{
+  Matrix Rh(3, 1);
+  double phi = _minPhi + ((double)beam) * _angularRes;
+  double cphi = cos(phi);
+  double sphi = sin(phi);
+  Rh[0][0] = cos(phi);
+  Rh[1][0] = sin(phi);
+  Rh[2][0] = 0.0;
+  Rh = (*_Pose) * Rh;
+  ray[0] = Rh[0][0];
+  ray[1] = Rh[1][0];
+}
 
-  int SensorPolar2D::backProject(double* data)
-  {
-    Matrix xh(3, 1);
-    xh[0][0] = data[0];
-    xh[1][0] = data[1];
-    xh[2][0] = 1.0;
-    Matrix PoseInv = (*_Pose);
-    PoseInv.invert();
-    xh = PoseInv * xh;
+int SensorPolar2D::backProject(double* data)
+{
+  Matrix xh(3, 1);
+  xh[0][0] = data[0];
+  xh[1][0] = data[1];
+  xh[2][0] = 1.0;
+  Matrix PoseInv = (*_Pose);
+  PoseInv.invert();
+  xh = PoseInv * xh;
 
-    double phi = atan2(xh[1][0], xh[0][0]);
-    return phi2Index(phi);
-  }
+  double phi = atan2(xh[1][0], xh[0][0]);
+  return phi2Index(phi);
+}
 
-  int SensorPolar2D::phi2Index(double phi)
-  {
-    double phiAligned = phi-_minPhi;
+int SensorPolar2D::phi2Index(double phi)
+{
+  double phiAligned = phi-_minPhi;
 
-    // ensure angle to lie in valid bounds
-    if(phiAligned<=-0.5*_angularRes) return -1;
-    if(phiAligned>=(2.0*M_PI-0.5*_angularRes)) phiAligned -= 2.0*M_PI;
+  // ensure angle to lie in valid bounds
+  if(phiAligned<=-0.5*_angularRes) return -1;
+  if(phiAligned>=(2.0*M_PI-0.5*_angularRes)) phiAligned -= 2.0*M_PI;
 
-    int index = round(phiAligned /_angularRes);
+  int index = round(phiAligned /_angularRes);
 
-    if(index >= _size) index = -1;
+  if(index >= _size) index = -1;
 
-    return index;
-  }
+  return index;
+}
 
 }
