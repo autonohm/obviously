e6db6e492334679d99b6eefa5f0e54c962635a30
knueppl <knueppl@ka640>
2014-07-07 17:58:36 +0200

    Just saved.

--- test/obcore/base/eigen-vs-gsl.cpp
+++ test/obcore/base/eigen-vs-gsl.cpp
@@ -4,13 +4,20 @@
 #include <unistd.h>
 #include <iostream>
 
+#include <Eigen/Core>
+
+namespace {
+const unsigned int DIM = 1000000;
+}
+
 int main(int argc, char** argv)
 {
-    obvious::Time start(obvious::Time::now());
     obvious::Timer timer;
     timer.start();
 
-    ::sleep(2);
-    std::cout << "elapsed time = " << obvious::Time::now() - start << std::endl;
+    {
+        Eigen::Matrix<double, Eigen::Dynamic, 3, Eigen::RowMajor> mat(DIM, 3);
+    }
+
     std::cout << "elapsed time = " << timer.elapsed() << std::endl;
 }
