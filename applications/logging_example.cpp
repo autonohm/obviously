cecbddfa5eac1dcf9a3658e0d0133957613be7b0
sm <sm@ohm>
2014-12-05 08:22:37 +0100

    minor change

--- applications/logging_example.cpp
+++ applications/logging_example.cpp
@@ -6,7 +6,7 @@ using namespace obvious;
 
 int main(int argc, char* argv[])
 {
-	LOGMSG_CONF("logging_example.log", Logger::file_on|Logger::screen_on, DBG_DEBUG, DBG_ERROR);
+	LOGMSG_CONF("logging_example", Logger::file_on|Logger::screen_on, DBG_DEBUG, DBG_ERROR);
 	LOGMSG(DBG_DEBUG, "hello " << "world");
 	LOGMSG(DBG_WARN, "something " << "strange");
 	LOGMSG(DBG_ERROR, "this is an error");
