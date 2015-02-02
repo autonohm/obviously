9beb1fb5404b04b9b4dda7eca8db4d5e95482e72
sm <sm@ohm>
2015-02-02 18:14:41 +0100

    - release compiler flags - commenting

--- obvision/reconstruct/grid/TsdGridComponent.h
+++ obvision/reconstruct/grid/TsdGridComponent.h
@@ -17,22 +17,60 @@ namespace obvious
 class TsdGridComponent
 {
 public:
+  /**
+   * Constructor
+   * @brief Abstraction layer for leafs and branches
+   * @param isLeaf discriminate leaf from branch
+   */
   TsdGridComponent(bool isLeaf);
 
+  /**
+   * Destructor
+   */
   virtual ~TsdGridComponent();
 
+  /**
+   * Edge length of component
+   * @return edge length
+   */
   obfloat getComponentSize();
 
+  /**
+   * Get coordinates of centroid
+   * @return coordinate array
+   */
   obfloat* getCentroid();
 
+  /**
+   * Get circumradius, i.e., diagonal length of component
+   * @return circumradius
+   */
   obfloat getCircumradius();
 
+  /**
+   * Get homogeneous coordinates of edges
+   * @return coordinate array
+   */
   Matrix* getEdgeCoordsHom();
 
+  /**
+   * Discriminate leaf from branch
+   * @return discrimination flag
+   */
   bool isLeaf();
 
+  /**
+   * Check whether component is in range from a certain sensor position
+   * @param pos sensor position
+   * @param sensor sensor
+   * @param maxTruncation maximum truncation radius
+   * @return visibility flag
+   */
   bool isInRange(obfloat pos[2], Sensor* sensor, obfloat maxTruncation);
 
+  /**
+   * Increase emptiness of component (for visible cells, in which every measurement ray passes through without hitting a surface)
+   */
   virtual void increaseEmptiness() = 0;
 
 protected:
--- obvision/reconstruct/grid/TsdGridPartition.h
+++ obvision/reconstruct/grid/TsdGridPartition.h
@@ -41,34 +41,100 @@ public:
    */
   TsdGridPartition(const unsigned int x, const unsigned int y, const unsigned int dimX, const unsigned int dimY, const obfloat cellSize);
 
+  /**
+   * Destructor
+   */
   ~TsdGridPartition();
 
+  /**
+   * TSD access operator
+   * @param y y-index
+   * @param x x-index
+   * @return TSD
+   */
   obfloat& operator () (unsigned int y, unsigned int x);
 
+  /**
+   * Initialize partitions
+   */
   void init();
 
+  /**
+   * Initialization indication
+   * @return initialization flag
+   */
   bool isInitialized();
 
+  /**
+   * Emptiness indication
+   * @return emptiness flag
+   */
   bool isEmpty();
 
+  /**
+   * Get x-index
+   * @return x-index
+   */
   unsigned int getX();
 
+  /**
+   * Get y-index
+   * @return y-index
+   */
   unsigned int getY();
 
+  /**
+   * Get homogeneous cell coordinates
+   * @return cell coordinate matrix of partition
+   */
   Matrix* getCellCoordsHom();
 
+  /**
+   * Get partition coordinates
+   * @return partition coordinates matrix
+   */
   Matrix* getPartitionCoords();
 
+  /**
+   * Get width, i.e., number of cells in x-dimension
+   * @return width
+   */
   unsigned int getWidth();
 
+  /**
+   * Get height, i.e., number of cells in y-dimension
+   * @return height
+   */
   unsigned int getHeight();
 
+  /**
+   * Get number of cells in partition, i.e., width x height
+   * @return number of cells
+   */
   unsigned int getSize();
 
+  /**
+   * Add TSD value at certain cell
+   * @param x cell x-index
+   * @param y cell y-index
+   * @param sdf SDF
+   * @param maxTruncation maximum truncation radius
+   */
   void addTsd(const unsigned int x, const unsigned int y, const obfloat sdf, const obfloat maxTruncation);
 
+  /**
+   * Increase emptiness of whole partition, i.e., every measurement ray passes through partition
+   */
   virtual void increaseEmptiness();
 
+  /**
+   * Interpolate bilinear within cell
+   * @param x x-index
+   * @param y y-index
+   * @param dx x-coordinate within cell
+   * @param dy y-coordinate within cell
+   * @return interpolated TSD
+   */
   obfloat interpolateBilinear(int x, int y, obfloat dx, obfloat dy);
 
 private:
