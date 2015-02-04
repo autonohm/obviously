47efe8386efa3f337566cd0dfcbd02e31c4f029e
sm <sm@ohm>
2015-02-04 17:08:06 +0100

    renamed robot footprint filter

--- obvision/icp/assign/filter/RobotFootprintFilter.h
+++ obvision/icp/assign/filter/RobotFootprintFilter.h
@@ -1,12 +1,12 @@
 /*
- * RobotFootprintFilter3D.h
+ * RobotFootprintFilter.h
  *
  *  Created on: 20.09.2013
  *      Author: chris
  */
 
-#ifndef ROBOTFOOTPRINTFILTER3D_H_
-#define ROBOTFOOTPRINTFILTER3D_H_
+#ifndef ROBOTFOOTPRINTFILTER_H_
+#define ROBOTFOOTPRINTFILTER_H_
 
 #include "obvision/icp/assign/filter/IPreAssignmentFilter.h"
 #include "obcore/math/mathbase.h"
@@ -14,40 +14,40 @@
 namespace obvious
 {
 
-class RobotFootprintFilter3D : public IPreAssignmentFilter
+class RobotFootprintFilter : public IPreAssignmentFilter
 {
 public:
   /**
    * Default constructor with radius mode
-   * @param minRadius
    * @param maxRadius
    */
-  RobotFootprintFilter3D(double minRadius = 0.0, double maxRadius = NAN);
+  RobotFootprintFilter(unsigned int dim, double maxRadius);
+
   /**
    * Constructor to initialize filter with a box
    * @param[in]         x_lim           min and max value for x axis
    * @param[in]         y_lim           min and max value for y axis
    * @param[in]         z_lim           min and max value for z axis
    */
-  RobotFootprintFilter3D(const double* x_lim,
+  /*RobotFootprintFilter(const double* x_lim,
                          const double* y_lim,
-                         const double* z_lim);
+                         const double* z_lim);*/
   /**
    * Default destructor
    */
-  ~RobotFootprintFilter3D();
+  ~RobotFootprintFilter();
   /**
    * Function to set offset
    * @param[in]          offset          offset in R3 coordinates
    */
-  void setOffset(const double* offset);
+  //void setOffset(const double* offset);
   /**
    * Function to set min and max radius
    * @param[in]         minRadius        minimal radius around offset
    * @param[in]         maxRadius        maximum radius around offset
    */
-  void setRadius(const double minRadius,
-                 const double maxRadius);
+  //void setRadius(const double minRadius,
+  //               const double maxRadius);
   /**
    * Virtual function of IPreAssignmentFilter
    * @param scene
@@ -55,13 +55,16 @@ public:
    * @param mask
    */
   virtual void filter(double** scene, unsigned int size, bool* mask);
+
 private:
-  double* _offset;
-  double  _minRadius;
+
+  unsigned int _dim;
+  //double* _offset;
+  //double  _minRadius;
   double  _maxRadius;
-  double* _x_lim;
-  double* _y_lim;
-  double* _z_lim;
+  //double* _x_lim;
+  //double* _y_lim;
+  //double* _z_lim;
 
 
 };
