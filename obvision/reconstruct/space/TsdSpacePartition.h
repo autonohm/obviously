6fa9a91eff748cfc88a26bde07d9da1b16880d33
Your Name <you@example.com>
2018-09-21 23:27:09 +0200

    added inline function to return a specific tsd value to an index.

--- obvision/reconstruct/space/TsdSpacePartition.h
+++ obvision/reconstruct/space/TsdSpacePartition.h
@@ -50,6 +50,7 @@ public:
   void reset();
 
   obfloat& operator () (unsigned int z, unsigned int y, unsigned int x) const { return _space[z][y][x].tsd; }
+  obfloat& getTsd(unsigned int z, unsigned int y, unsigned int x) const { return _space[z][y][x].tsd; }
 
   void getRGB(unsigned int z, unsigned int y, unsigned int x, unsigned char rgb[3]);
 
